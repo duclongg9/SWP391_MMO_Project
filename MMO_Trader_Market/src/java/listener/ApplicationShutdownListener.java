019899c7bb937435d87cfbddb523ae6bc43dc388
duclongg9 <68913318+duclongg9@users.noreply.github.com>
2025-10-26 19:11:58 +0700

    Add listener to clean up MySQL resources on shutdown

---
+++ MMO_Trader_Market/src/java/listener/ApplicationShutdownListener.java
@@ -0,0 +1,50 @@
+package listener;
+
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Ensures JDBC resources that are registered by the application are
+ * released when the web application is stopped to prevent memory leaks.
+ */
+@WebListener
+public class ApplicationShutdownListener implements ServletContextListener {
+
+    private static final Logger LOGGER = Logger.getLogger(ApplicationShutdownListener.class.getName());
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        shutdownAbandonedCleanupThread();
+        deregisterJdbcDrivers();
+    }
+
+    private void shutdownAbandonedCleanupThread() {
+        try {
+            AbandonedConnectionCleanupThread.checkedShutdown();
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+            LOGGER.log(Level.WARNING, "Interrupted while shutting down MySQL abandoned connection cleanup thread", ex);
+        }
+    }
+
+    private void deregisterJdbcDrivers() {
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(driver);
+                LOGGER.log(Level.FINE, "Deregistered JDBC driver {0}", driver);
+            } catch (SQLException ex) {
+                LOGGER.log(Level.WARNING, "Unable to deregister JDBC driver " + driver, ex);
+            }
+        }
+    }
+}
